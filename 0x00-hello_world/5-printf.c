dda8600a239a8404899b346a41aac47779f2f382
Duduzo <edgedefinitive21@gmail.com>
2023-06-19 07:07:02 -0700

    5-print.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,17 +1,12 @@
-uth: Brennan D Baraban 
-  */ 
-   
-  #include <stdio.h> 
-   
-  /** 
-   *   * main - Prints "with proper grammar, but the outcome is 
-   *    *                a piece of art," followed by a new line. 
-   *     * 
-   *      * Return: Always 0. 
-   *       */ 
-  int main(void) 
-	 { 
-		          printf("with proper grammar, but the outcome is a piece of art,\n"); 
-		    
-		           return (0); 
-		    }
+#include <stdio.h>
+/**
+ * main - print output with printf 
+ *
+ * Return: always (0)
+ *
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
